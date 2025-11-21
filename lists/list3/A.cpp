28ca1418cb5efc244722c3b70eff39fc008662fb
Matheus-Adiel <matheuus.adiiel@gmail.com>
2025-11-21 15:56:13 -0300

    add: question A.cpp of the list 3

--- lists/list3/A.cpp
+++ lists/list3/A.cpp
@@ -2,17 +2,40 @@
 
 using namespace std;
 
+long long int production (long long *machines, long long &size, long long int &temp, long long int &goal) {
+    long long int sumProducts = 0;
+    for (int i = 0; i < size; i++) {
+       sumProducts += temp/machines[i];
+       if (sumProducts >= goal)
+           break;
+    }
+    return sumProducts;
+}
+
 int main() {
 
-    int number_machines, product_goal, ini, end;
-    int* machines;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    cin >> number_machines >> product_goal;
-    machines = new int[number_machines];
+    long long static MAX_TEMP = 1e18;
+    long long goal, mid, ini = 0, end = MAX_TEMP;
+    long long number_machines;
+    long long* machines;
 
-    while (ini > end) {
+    cin >> number_machines >> goal;
+    machines = new long long[number_machines];
 
-  
-    } 
+    for (int i = 0; i < number_machines; i++)
+        cin >> machines[i];
+
+    while (ini < end) { 
+        mid = (ini + end) / 2;
+        if (production(machines, number_machines, mid, goal) < goal)
+            ini = mid + 1;
+        else 
+            end = mid;
+    }
+
+    cout << ini << "\n";
 
 }
